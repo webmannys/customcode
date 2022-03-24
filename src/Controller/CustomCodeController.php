7647e3b3135c244f185dbc47c03278ad2bbe4d4c
Manny Sangha <webmannys@gmail.com>
2022-03-23 17:55:24 -0700

    Added custom page in the controller and create route for the page.

--- src/Controller/CustomCodeController.php
+++ src/Controller/CustomCodeController.php
@@ -5,3 +5,24 @@
  */
 
 namespace Drupal\customcode\Controller;
+use Drupal\Core\Controller\ControllerBase;
+
+
+/**
+ * CustomCodeController
+ */
+class CustomCodeController extends ControllerBase {
+
+	//For Complete Chargesheet
+  public function page() {
+		
+		$myhtml = "<h1>My Custom Page</h1>";
+		$myhtml .= "<p>This is my awesome custom page with html</p>";
+		
+		$build = [
+					'#markup' => $this->t($myhtml),
+		];
+		
+    return $build;
+  }
+}
